aa9d990c1171c536016de3246a59e3a1eddd4535
AlbertKoii <a666523104@gmail.com>
2026-02-07 16:29:56 +0800

    renew and learn gemini api key

--- geminiDemo_0207.py
+++ geminiDemo_0207.py
@@ -4,5 +4,18 @@ import os
 from google import genai
 
 load_dotenv()
-url = os.getenv("geminiKey")
 
+url = os.getenv("geminiKey")
+prompts = ""
+client = genai.Client(api_key = url)
+
+def conversation (): 
+    client.models.generate_content (
+        model = "Gemini 2.5 Flash-Lite",
+        content = prompts,
+        config={
+            "system_instruction": "你是一個專業的技術顧問，請簡短回答。"
+        }
+    )
+    
+    
